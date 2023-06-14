b876b5c99cbc6a4ebba0bc0595533e9d0441adc1
Anant Tater - ITT <anantideapad@gmail.com>
2023-06-14 13:00:32 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,61 +1,64 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+const https = require("https");
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.post("/", function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const userEmail = req.body.email;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    const data = {
+        email_address: userEmail,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+        }
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us21.api.mailchimp.com/3.0/lists/a7a1d0ae3f/members"
+    const options = {
+        method: "POST",
+        auth: "ananttater:52ebd03a15753354eef67f58e4cb2eeb-us21"
+    }
+
+    const request = https.request(url, options, function(responce){
+
+        if(responce.statusCode === 200){
+            res.sendFile(__dirname+"/success.html");
+        } else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+
+        responce.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 1805, function(){
+    console.log("Server is up & running on 1805 port.")
+});
+
+// audience_id = a7a1d0ae3f
+// var api_key = "52ebd03a15753354eef67f58e4cb2eeb-us21";
